d059ec42282ab9a86678fb6124baeba4f55b2d90
Angelica-rwill <angelicarwill@gmail.com>
2025-03-12 13:52:42 +0000

    Novaalteração.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -38,5 +38,44 @@ int main() {
     printf("Area: %f - PIB: %f\n", Area, PIB);
     printf("Pontos turisticos: %d\n", PontosTuristicos);
 
+    printf("\nDesafio super trunfo");
+    printf("\nCarta 2:\n");
+
+    char Estado2 [20]; 
+    char Codigo2 [20];
+    char Cidade2 [10];
+    int Populacao2;
+    float Area2;
+    float PIB2;
+    int PontosTuristicos2;
+
+    printf("Digite o Estado2: \n");
+    scanf("%s", Estado2);
+
+    printf("Digite o Codigo2: \n");
+    scanf("%s", Codigo2); 
+
+    printf("Digite a Cidade2: \n");
+    scanf("%s", Cidade2);
+
+    printf("Digite o número da Populaçao2: \n");
+    scanf("%d", &Populacao2);
+
+    printf("Digite a Area2: \n");
+    scanf("%f", &Area2);
+
+    printf("Digite o PIB2: \n");
+    scanf("%f", &PIB2);
+
+    printf("Digite o número de pontos turisticos2: \n");
+    scanf("%d", &PontosTuristicos2);
+
+    printf("Estado2: %s - Codigo2: %s - Cidade2: %s\n", Estado2, Codigo2, Cidade2);
+    printf("Populacao2: %d\n", Populacao2);
+    printf("Area2: %f - PIB2: %f\n", Area2, PIB2);
+    printf("Pontos turisticos2: %d\n", PontosTuristicos2);
+
     return 0;
 }
+
+    
